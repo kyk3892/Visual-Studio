eeb8dd21d8c1b49aac709140fadf34996dea3271
kyk3892 <kyk3892@naver.com>
2020-05-13 02:03:06 +0900

    formon추가

---
+++ Formon/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Formon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 0;
+            int[] intArray = { 10, 20, 30, 7, 3 };
+
+            while (i < intArray.Length)
+            {
+                Console.WriteLine(i + "번째 출력 :" + intArray[i]);
+
+                i++;
+            }
+
+
+        }
+    }
+}
